b80b1ca45b96dfa8d3e10d7d02b6eb0471405144
RyanPCo <copierceryan@gmail.com>
2024-10-13 12:17:38 -0400

    Added study and remove actions

--- Spaced Repetition.py
+++ Spaced Repetition.py
@@ -1,3 +1,4 @@
+import sqlite3
 class Flashcard:
     def __init__(self, front, back):
         self.front = front
@@ -6,10 +7,28 @@ class Flashcard:
     def __str__(self) -> str:
         return f"Front: {self.front}\nBack: {self.back}"
     
-response = input("Actions: study, add, remove\n")
+cards = []
+score = 0
 
-if response == "add":
-    front = input("Front of card: ")
-    back = input("Back of card: ")
-    card1 = Flashcard(front, back)
+while True:
+    response = input("Actions: study, add, remove\n")
 
+    if response == "add":
+        front = input("Front of card: ")
+        back = input("Back of card: ")
+        cards.append(Flashcard(front, back))
+    elif response == "study":
+        for i in cards:
+            print(i.front)
+            answer = input("Answer: ")
+            if answer == i.back:
+                score = score + 1
+            while answer != i.back:
+                answer = input("Incorrect, try again: ")
+            print("Correct!")
+        print("Your score was ", score, "/", len(cards))
+    elif response == "remove":
+        for i in cards:
+            print(i)
+        answer = input(f"Here is a list of your flashcards, which one would you like to remove?\n")
+        cards.remove(answer)
